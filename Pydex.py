e9a6d0a43b3bab870589c5ce1572820f56559c08
Nadav Chen <nadav2282@gmail.com>
2020-11-11 11:34:34 +0200

    Added Wikipedia as database and a while loop to Pydex.py

--- Pydex.py
+++ Pydex.py
@@ -1,12 +1,19 @@
 import wolframalpha
+import wikipedia
 
-user_input = input("Ask a question: ")
-app_id = "E7H2EU-267UWJ7XRP"
+while True:
+    user_input = input("Ask a question: ")
+    try:
+        # Based on Wolfram Alpha
+        app_id = "E7H2EU-267UWJ7XRP"
+        client = wolframalpha.Client(app_id)
+        result = client.query(user_input)
+        answer = next(result.results).text
+        print(answer)
+    except:
+        # Based on Wikipedia
+        # wikipedia.set_lang("") - If you want to change language
+        print(wikipedia.summary(user_input, sentences=2))
 
-client = wolframalpha.Client(app_id)
 
-result = client.query(user_input)
-answer = next(result.results).text
-
-print(answer)
 
